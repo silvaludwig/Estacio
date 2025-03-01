d9ee66e9f0957170676220bc4d6508d694c223e2
Ludwig S <silvaludwig@outlook.com>
2025-03-01 07:54:22 -0300

    implementando movimento do cavalo com loops aninhados

--- introducaoLinguagemC/parte3/desafioXadrez.cpp
+++ introducaoLinguagemC/parte3/desafioXadrez.cpp
@@ -11,11 +11,16 @@ Falando do que ficou pronto:
 gostaria de movimentar. Por enquanto as peças tem movimentos pre-definidos, 
 conforme foi solicitado no exercício. Mas a ideia é que, nas próximas versões, 
 o próprio usuário possa escolhar a direção e a quantidade de casas movimentadas. 
+
+Na movimentação do cavalo eu copiei a do professor, porém só depois de tentar fazer.
+Tentei usar um for aninhado a outro for e não consegui fazer funcionar ainda. 
+Uma hora a saída ficou DIREITA CIMA CIMA, outra ficou DIREITA CIMA DIREITA CIMA kkkkkk
 */
 
 int main(){
 
     int escolhaUsuario;
+    int movimentoCompleto = 1;
 
     printf("movimentando peças de xadrez\n");
     printf("por enquanto as peças tem movimentos pre-definidos\n");
@@ -24,6 +29,7 @@ int main(){
     printf("1. Torre \n");
     printf("2. Bispo \n");
     printf("3. Rainha \n");
+    printf("4. Cavalo \n");
     printf("Qual peça vc quer mover? \n");
     scanf(" %d", &escolhaUsuario);
 
@@ -70,11 +76,36 @@ int main(){
             //     a++;
             // }
             break;
+        
+        case 4:
+            printf("vc escolheu o Cavalo, ele se movimenta em L\n");
+            printf("nesse exercício ele se moverá pra cima duas vezes e uma vez pra direita\n");
+
+            while (movimentoCompleto--)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        printf("Cima\n");
+                    }
+                    printf("Direita\n");
+                }
+            
+
+            // for (int i = 0; i < 2; i++)
+            // {
+            //     printf("Pra cima\n");
+            //     for (int j = 0; j < 1; j++)
+            //     {
+            //         printf("Direita\n");
+            //     }               
+            // }
+            break;
                     
         default:
             printf("nenhuma opção válida");
             break;
-        }
+        
+    }
 
 
     
