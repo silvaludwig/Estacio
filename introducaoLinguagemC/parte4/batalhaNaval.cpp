c91f19d877474b3c929486133e5216442a1ad3bf
Ludwig S <silvaludwig@outlook.com>
2025-03-18 17:01:29 -0300

    batalha naval

---
+++ introducaoLinguagemC/parte4/batalhaNaval.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int tamanho = 5;
+    int tabuleiro[5][5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("a");
+        for (int j = 0; i < 5; j++)
+        {
+            printf("o");
+        }
+        
+    }
+    
+    return 0;
+}
